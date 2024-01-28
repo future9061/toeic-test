701819969521e3de115679c81b30b1510f9fac43
leemirae <416homin@daum.net>
2024-01-28 22:15:00 +0900

    pwa ios 호환
    - 내용적는곳1
    - 내용 길어질때 내용적는곳

--- frontend/src/PWAinstallPrompt.tsx
+++ frontend/src/PWAinstallPrompt.tsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { PWAInstallPromptCSS } from './style/PWAinstallPromptCSS';
+import Mascort from './components/common/Mascort';
 
 function PWAinstallPrompt() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [defferedPrompt, setDefferedPrompt] = useState<any>(null);
 
+  const [isShown, setIsShown] = useState(false);
+  const [isIOS, setIsIOS] = useState(false);
+
   useEffect(() => {
+    const isDeviceIOS = /iPad|iPhone|iPod/.test(window.navigator.userAgent);
+    setIsIOS(isDeviceIOS);
+
     window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
 
     return () => {
@@ -19,9 +26,12 @@ function PWAinstallPrompt() {
   const handleBeforeInstallPrompt = (e: Event) => {
     e.preventDefault();
     setDefferedPrompt(e);
+    setIsShown(true);
   };
 
   const handleInstallClick = () => {
+    setIsShown(false);
+
     if (defferedPrompt) {
       defferedPrompt.prompt();
 
@@ -37,13 +47,25 @@ function PWAinstallPrompt() {
     }
   };
 
-  return (
+  const content = (
     <PWAInstallPromptCSS>
-      {defferedPrompt && (
+      <Mascort />
+      <p>
+        <span>TOEIC</span>은 앱에서 원할하게 사용할 수 있어요.
+      </p>
+      <strong>앱을 설치하시겠습니까?</strong>
+      <div>
         <button onClick={handleInstallClick}>앱 다운 받기 ↓</button>
-      )}
+        <button onClick={() => setDefferedPrompt(null)}>괜찮아요 :)</button>
+      </div>
     </PWAInstallPromptCSS>
   );
+
+  if (!isIOS && !isShown) {
+    return null;
+  }
+
+  return <>{defferedPrompt && content}</>;
 }
 
 export default PWAinstallPrompt;
