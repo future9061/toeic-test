eff1b107d97a74913233d8b0bf23240d5ef7c111
leemirae <416homin@daum.net>
2024-01-29 16:50:29 +0900

    pwa test
    - 내용적는곳1
    - 내용 길어질때 내용적는곳

---
+++ frontend/src/components/pwaPrompt/DesktopPrompt.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '../common/Button';
+
+function DesktopPrompt() {
+  return (
+    <>
+      <div className="line"></div>
+      <Button children="앱 다운받기 ↓" bgcolor="#2E66DD" />
+    </>
+  );
+}
+
+export default DesktopPrompt;
