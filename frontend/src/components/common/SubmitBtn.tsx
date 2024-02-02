a5637a342bf8863815d2a4c3fa23274d5acc6580
leemirae <416homin@daum.net>
2024-02-02 12:05:53 +0900

    sdocs : commit 템플릿 적용
    - 내용적는곳1
    - 내용 길어질때 내용적는곳

--- frontend/src/components/common/SubmitBtn.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { SubmitBtnCSS } from '../../style/components/common/SubmitBtnCSS';
-import { SubmitBtnProps } from '../../types/SubmitBtnProps';
-
-function SubmitBtn({ children, bgColor, color }: SubmitBtnProps) {
-  return <SubmitBtnCSS children={children} bgColor={bgColor} color={color} />;
-}
-
-export default SubmitBtn;
--- frontend/src/style/pages/LoginCSS.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const LoginCSS = styled.div`
-  min-height: 100vh;
-  text-align: center;
-  background-image: url('./img/bg-img.webp');
-`;
